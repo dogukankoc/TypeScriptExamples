7a3154307f65a2e4e3713379902b67b9185b9244
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-08-17 21:32:55 +0300

    CreatedTypeAnnotationsAndTypeInference

---
+++ TypeAnnotationsAndTypeInference/app.ts
@@ -0,0 +1,29 @@
+//Type Annotation
+let counter: number;
+counter = 1;
+
+let anotherCounter: number = 2;
+
+let firstName: string = 'Dogukan';
+let age: number = 26;
+let isTrue: boolean = true;
+
+//Array
+let names: string[] = ['Dogukan','John','Micheal','David']; 
+
+//Object
+let person: {
+    name: string,
+    age: number
+};
+
+person = {
+    name: 'John',
+    age: 26
+};
+
+//Type Inference
+let lastName = 'Koc';
+
+
+
